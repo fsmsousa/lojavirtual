1a9008e11e7ca54c461353ef42fc158601810b73
fsmsousa <fsmsousa@outlook.com>
2016-03-23 19:14:43 -0300

    teste_1

---
+++ lojavirtualFsmsousa/Modelo/Clientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Clientes
+    {
+        public void Salvar(Cliente cliente)
+        {
+            if (string.IsNullOrEmpty(cliente.Nome))
+            {
+                throw new ArgumentException(" Nome nao pode ser nulo. ");
+            }
+
+        }
+    }
+}
---
+++ lojavirtualFsmsousa/TestModelo/TestSlavaCliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Modelo;
+
+namespace TestModelo
+{
+    [TestClass]
+    public class TestSalvaCliente
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DevLancarExcecaoONomeForNulo()
+        {
+            var cliente = new Cliente();
+
+            new Clientes().Salvar(cliente);
+            
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DevLancarExcecaoONomeForVazio()
+        {
+            var cliente = new Cliente()
+            {
+                Nome = ""
+            };
+
+            new Clientes().Salvar(cliente);
+
+        }
+
+
+    }
+}
