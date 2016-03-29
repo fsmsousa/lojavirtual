af8190e410c7929a16a2f6ab918752949dd2408a
fsmsousa <fsmsousa@outlook.com>
2016-03-28 21:44:54 -0300

    teste_3

---
+++ lojavirtualFsmsousa/TestLojaVirtual/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Modelo;
+using lojavirtualFsmsousa.Models;
+using AutoMapper;
+using lojavirtualFsmsousa.Mappers;
+
+namespace TestLojaVirtual
+{
+    [TestClass]
+    public class TestProdutoMapper
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            AutoMapperConfig.RegisterMappings();
+        }
+
+        [TestMethod]
+
+        public void DeveMapearDeProdutoParaProdutoViewModel()
+        {
+            var produto = new Produto
+            {
+                Nome = "Chuteira Nike",
+                Categoria = new Categoria
+                {
+                    Nome = "Calçados"
+                }
+            };
+            var produtoViewModel = Mapper.Map<ProdutoViewModel>(produto);
+
+            Assert.IsNotNull(produtoViewModel);
+        }
+        [TestMethod]
+        public void DeveMapearNomeDoProduto()
+        {
+            var produto = new Produto
+            {
+                Nome = "Chuteira Nike",
+                Categoria = new Categoria
+                {
+                    Nome = "Calçados"
+                }
+            };
+            var produtoViewModel = Mapper.Map<ProdutoViewModel>(produto);
+
+            Assert.AreEqual("Chuteira Nike", produtoViewModel.Nome);
+        }
+        [TestMethod]
+        public void DeveMapearNomeDaCategoria()
+        {
+            var produto = new Produto
+            {
+                Nome = "Chuteira Nike",
+                Categoria = new Categoria
+                {
+                    Nome = "Calçados"
+                }
+            };
+            var produtoViewModel = Mapper.Map<ProdutoViewModel>(produto);
+
+            Assert.AreEqual("Calçados", produtoViewModel.Categoria);
+        }
+    }
+}
---
+++ lojavirtualFsmsousa/lojavirtualFsmsousa/Mappers/ProdutoViewModelMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using lojavirtualFsmsousa.Models;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lojavirtualFsmsousa.Mappers
+{
+    public class ProdutoViewModelMappingProfile : Profile
+    {
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Produto, ProdutoViewModel>()
+                .ForMember(dest => dest.Categoria, opt => opt.MapFrom(src => src.Categoria.Nome));
+        }
+    }
+}
